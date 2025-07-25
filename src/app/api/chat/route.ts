bc5f151956da6f539e619d2c57046fb9eb766d5d
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2025-07-25 13:04:10 -0500

    fix: test hook of ai sdk

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,9 +6,18 @@ export const maxDuration = 30;
 
 export async function POST(req: NextRequest) {
   const { messages } = await req.json();
+  console.log(messages);
   const result = streamText({
     model: google("gemini-2.0-flash"),
-    system: "",
+    system:
+      "You are a helpful assistant with deep knowledge in psychological therapies using music as a tool. " +
+      "Your work is recommended to people a list of twenty (20) songs that are suitable for the current situation and give a message to the user about your situation. " +
+      "Songs must be selected to help the user feel better or to help him/her to relax or to focus. " +
+      "Each song must have a 'title' and an 'artist'. " +
+      "You must detect what is the language of the user like English, Spanish, French, etc. and your response must be in that language. " +
+      "The title of the song must be in real language and the artist should be in real language. " +
+      "You must detect the emotion of the user and select the songs that match the emotion. " +
+      "You must show the user what emotions you detected and the songs you selected. ",
     temperature: 0.3,
     maxRetries: 3,
     messages,
--- src/components/bubble-chat.tsx
+++ src/components/bubble-chat.tsx
@@ -15,33 +15,21 @@ export default function BubbleChat({
 								key={index}
 								className={`flex flex-col gap-2 ${item.role === "user" ? "justify-end" : "justify-start"}`}
 							>
-								<p className={`text-archivo p-2 ${item.role === "user" ? "w-fit self-end text-right rounded-xl bg-gray-light/30 dark:bg-gray-light/20" : "text-left"}`}>{item.content}</p>
-								<div
-									className="flex flex-col gap-2 items-center justify-start overflow-x-auto"
-								>
-									{
-										item.songs && item.songs.length > 0 && item.songs.map((song, index) => {
+								{
+									item.parts?.map((part, index) => {
+										console.log("Parte", part);
+										if (part.type === "text") {
 											return (
-												<div
-													className="w-full p-2 flex gap-2 border border-gray-light dark:border-gray-light-opacity/20 rounded-xl"
+												<p
 													key={index}
+													className={`text-archivo p-2 ${item.role === "user" ? "w-fit self-end text-right rounded-xl bg-gray-light/30 dark:bg-gray-light/20" : "text-left"}`}
 												>
-													<span>
-														<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#1DB954" d="M21 3v12.5a3.5 3.5 0 0 1-3.5 3.5a3.5 3.5 0 0 1-3.5-3.5a3.5 3.5 0 0 1 3.5-3.5c.54 0 1.05.12 1.5.34V6.47L9 8.6v8.9A3.5 3.5 0 0 1 5.5 21A3.5 3.5 0 0 1 2 17.5A3.5 3.5 0 0 1 5.5 14c.54 0 1.05.12 1.5.34V6z"/></svg>
-													</span>
-													<div className="flex flex-col gap-0.5">
-														<p className="font-montserrat font-medium text-black dark:text-white text-left">
-															{song.title}
-														</p>
-														<span className="font-archivo text-sm text-gray-600 dark:text-gray-light text-left">
-															{song.artist}
-														</span>
-													</div>
-												</div>
+													{part.text}
+												</p>
 											)
-										})
-									}
-								</div>
+										}
+									})
+								}
 							</div>
 						)
 					})
